4f7877c2038f606dfd6ccaf93e0c37790b725a59
zhakina90 <zhakina90@gmail.com>
2018-12-16 21:47:48 -0600

    Added a few lines to console.log to make more readable for users.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -47,29 +47,40 @@ function userChoice() {
             //displaying the result of user input
             var id = answers.item_id;
             var quantity = answers.unit_products;
-            console.log(id);
-            console.log(quantity);
+            console.log("*******************************************************************");
+
+            console.log("Chosen Product ID: " + id);
+            console.log("*********************************************************************");
+
+            console.log("The Total amount of Products Selected: " + quantity);
+            console.log("*********************************************************************");
+
 
             connection.query("select * from products where item_id =" + id, function (err, response) {
                 console.log(response);
                 if (err) throw err;
                 //checking if we have user's request in stock
                 if (response[0].stock_quantity - quantity <= 0) {
-                    console.log("Insufficient quantity!");
+                    console.log("############INSUFFICIENT QUANTITY!!!############");
+
+
+
                 } else {
-                    console.log("The Order Succsefully placed");
+                    console.log("*********************YOUR ORDER IS  SUCCESSFULLY PLACED!!!!****************");
+                    // connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [response[0].stock_quantity - quantity, id],
+                    //     function (error, resp) {
+                    //         if (error) throw error;
+                    //         // console.log(resp);
+                    //     });
+
                 }
 
-            })
-            connection.query('UPDATE products SET stock_quantity=? WHERE id=?', [response[0].stock_quantity - quantity], function () {
-
-            }
-
+            });
 
         });
 
 
-    })
+    });
 }
 
 
@@ -77,28 +88,25 @@ function userChoice() {
 
 //applie npm console.log to display data in better way
 var values = [
-    [1, "Printer", "Electronics", 45, 50],
-    [2, "Red Skirt", "Clothes", 20, 100],
-    [3, "White Socks", "Clothes", 4.5, 150],
-    [4, "Black Shirt", "Clothes", 12.4, 50],
-    [5, "Dums", "Toys", 3.4, 200],
-    [6, "Barbie", "Toys", 34.7, 500],
-    [7, "Cars", "Toys", 1.34, 290],
-    [8, "Pillow", "Home", 7.90, 140],
-    [9, "Blanket", "Home", 10, 80],
-    [10, "Tide", "Household", 12.9, 400],
-    [11, "Dove Soap", "Household", 0.9, 1000],
-    [12, "Computer mouse", "Electronics", 5.67, 200],
-    [13, "Face Cream", "Beauty", 34.6, 40],
-    [14, "Red Lip-Stick", "Beauty", 10.00, 35],
-    [15, "Pink Blush", "Beauty", 24.7, 25]
+    [1, "Camera", "Electronics", 60, 80],
+    [2, "Printer", "Electronics", 45, 50],
+    [3, "Red Skirt", "Clothes", 20, 100],
+    [4, "White Socks", "Clothes", 4.5, 150],
+    [5, "Black Shirt", "Clothes", 12.4, 50],
+    [6, "Dums", "Toys", 3.4, 200],
+    [7, "Barbie", "Toys", 34.7, 500],
+    [8, "Cars", "Toys", 1.34, 290],
+    [9, "Pillow", "Home", 7.90, 140],
+    [10, "Blanket", "Home", 10, 80],
+    [11, "Tide", "Household", 12.9, 400],
+    [12, "Dove Soap", "Household", 0.9, 1000],
+    [13, "Computer mouse", "Electronics", 5.67, 200],
+    [14, "Face Cream", "Beauty", 34.6, 40],
+    [15, "Red Lip-Stick", "Beauty", 10.00, 35],
+    [16, "Pink Blush", "Beauty", 24.7, 25]
 
 ]
 console.table(["item_id", "product_name", "department_name", "price", "stock_quantity"], values);
-
-
-
-
 
 
 userChoice();
